13e72017af47b8b62a9c2dc8ebf0d0de17a33b67
sudosuanjal <anjalehackchey101@gmail.com>
2025-12-12 11:30:46 +0530

    getterfunction for entry fees

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -5,6 +5,9 @@ contract Lottery {
     error Lottery_EntryFeesNotEnough();
 
     uint256 private immutable i_entryFees;
+    address payable[] s_players;
+
+    event PlayerEnteredLottery(address indexed player);
 
     constructor(uint256 entryFees) {
         i_entryFees = entryFees;
@@ -14,7 +17,13 @@ contract Lottery {
         if (msg.value < i_entryFees) {
             revert Lottery_EntryFeesNotEnough();
         }
+        s_players.push(payable(msg.sender));
+        emit PlayerEnteredLottery(msg.sender);
     }
 
     function pickWinner() public {}
+
+    function getEntryFees() external view returns (uint256) {
+        return i_entryFees;
+    }
 }
