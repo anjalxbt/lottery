fe4d110185ed6c7f37a3afc16c14c88bf54693e1
sudosuanjal <anjalehackchey101@gmail.com>
2025-12-15 14:39:11 +0530

    some refactoring

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -4,26 +4,34 @@ pragma solidity ^0.8.19;
 contract Lottery {
     error Lottery_EntryFeesNotEnough();
 
-    uint256 private immutable i_entryFees;
+    uint256 private immutable I_ENTRY_FEES;
     address payable[] s_players;
+    uint256 immutable i_interval;
+    uint256 private s_lastTimeStamp;
 
     event PlayerEnteredLottery(address indexed player);
 
-    constructor(uint256 entryFees) {
-        i_entryFees = entryFees;
+    constructor(uint256 entryFees, uint256 interval) {
+        I_ENTRY_FEES = entryFees;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterLottery() public payable {
-        if (msg.value < i_entryFees) {
+        if (msg.value < I_ENTRY_FEES) {
             revert Lottery_EntryFeesNotEnough();
         }
         s_players.push(payable(msg.sender));
         emit PlayerEnteredLottery(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+    }
 
     function getEntryFees() external view returns (uint256) {
-        return i_entryFees;
+        return I_ENTRY_FEES;
     }
 }
