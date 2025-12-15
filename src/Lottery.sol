ae827704541bf2013db78cc6333fb71a766b1242
sudosuanjal <anjalehackchey101@gmail.com>
2025-12-15 20:22:59 +0530

    done random word req

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,20 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Lottery {
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract Lottery is VRFConsumerBaseV2Plus {
     error Lottery_EntryFeesNotEnough();
 
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable I_ENTRY_FEES;
-    address payable[] s_players;
+    uint256 private immutable i_subscriptionId;
     uint256 immutable i_interval;
+    uint32 private immutable i_callbackGasLimit;
+    bytes32 private immutable i_keyHash;
     uint256 private s_lastTimeStamp;
+    address payable[] s_players;
 
     event PlayerEnteredLottery(address indexed player);
 
-    constructor(uint256 entryFees, uint256 interval) {
+    constructor(
+        uint256 entryFees,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         I_ENTRY_FEES = entryFees;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterLottery() public payable {
@@ -29,7 +47,19 @@ contract Lottery {
         if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
+        });
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {}
 
     function getEntryFees() external view returns (uint256) {
         return I_ENTRY_FEES;
